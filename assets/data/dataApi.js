076b20951e7ef6620587aa82553a49f9cb2af1bd
Tanish Raj <tanish.quitegrowth@gmail.com>
2020-06-05 01:55:08 +0530

    Completed assignment.

---
+++ assets/data/dataApi.js
@@ -0,0 +1,147 @@
+const dataApi = [
+    {
+        categoryType: "cleanser",
+        allProducts: [{
+            id: "cleanser1",
+            productImage: "./assets/staticImages/cleanser1.png",
+            productName: "Advanced Night Micro Cleansing Balm 1",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "32.00"
+        },
+        {
+            id: "cleanser2",
+            productImage: "./assets/staticImages/cleanser2.png",
+            productName: "Advanced Night Micro Cleansing Balm 2",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "25.00"
+        },
+        {
+            id: "cleanser3",
+            productImage: "./assets/staticImages/cleanser3.png",
+            productName: "Advanced Night Micro Cleansing Balm 3",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "30.00"
+        },
+        {
+            id: "cleanser4",
+            productImage: "./assets/staticImages/cleanser1.png",
+            productName: "Advanced Night Micro Cleansing Balm 4",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "40.00"
+        },
+        {
+            id: "cleanser5",
+            productImage: "./assets/staticImages/cleanser2.png",
+            productName: "Advanced Night Micro Cleansing Balm 5",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "50.00"
+        }]
+    },
+
+    {
+        categoryType: "serum",
+        allProducts: [{
+            id: "serum1",
+            productImage: "./assets/staticImages/serum4.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "100.00"
+        },
+        {
+            id: "serum2",
+            productImage: "./assets/staticImages/serum2.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "50.00"
+        },
+        {
+            id: "serum3",
+            productImage: "./assets/staticImages/serum3.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "40.00"
+        },
+        {
+            id: "serum4",
+            productImage: "./assets/staticImages/serum4.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "60.00"
+        },
+        {
+            id: "serum5",
+            productImage: "./assets/staticImages/serum1.jpg",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "70.00"
+        }]
+    },
+    {
+        categoryType: "eyeCream",
+        allProducts: [{
+            id: "eyeCream1",
+            productImage: "./assets/staticImages/serum4.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "80.00"
+        },
+        {
+            id: "eyeCream2",
+            productImage: "./assets/staticImages/serum2.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "10.00"
+        },
+        {
+            id: "eyeCream3",
+            productImage: "./assets/staticImages/serum3.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "60.00"
+        },
+        {
+            id: "eyeCream4",
+            productImage: "./assets/staticImages/serum4.png",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "32.00"
+        },
+        {
+            id: "eyeCream5",
+            productImage: "./assets/staticImages/serum1.jpg",
+            productName: "Advanced Night Micro Cleansing Balm",
+            briefDescription: "Radiant energy 2-in-1 cleansing foam",
+            productRating: 5,
+            productUsage: "Purifies skin in 2 ways: as daily cleanser or as mask",
+            productPrice: "32.00"
+        }]
+    }
+]
---
+++ js/accordion.js
@@ -0,0 +1,142 @@
+// Function for creating custom accordion
+(function customAccordion() {
+    var acc = $(".accordion");
+    var i;
+    for (i = 0; i < acc.length; i++) {
+        acc[i].addEventListener("click", function () {
+            var panel = this.nextElementSibling;
+            for (let j = 0; j < acc.length; j++) {
+                acc[j].classList.remove('active');
+                editSelectedProducts(acc[j], 'Select +');
+            }
+            document.querySelectorAll('.panel').forEach(item => {
+                if (item != panel)
+                    item.style.maxHeight = null;
+            })
+            this.classList.add("active");
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+                this.classList.remove("active");
+                editSelectedProducts(this, 'Select +');
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + "px";
+                editSelectedProducts(this, 'close x');
+            }
+        });
+    }
+})();
+
+function editSelectedProducts(params, params2) {
+    if ($(params).find('.product-description')[0].innerHTML === "")
+        $(params).find('.access-button').text(params2);
+    else
+        $(params).find('.access-button').text('Edit +');
+}
+
+function preapareDOM(data) {
+    let productHTML = `
+    <div class="carousel-cell">
+        <div class="item-description" data-product="${data.id}">
+            <img src=${data.productImage} />
+            <div class="other-details">
+                <div class="product-name">${data.productName}</div>
+                <div class="brief-descrption">${data.briefDescription}</div>
+                <div class="product-rating">
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star"></span>
+                    <span class="fa fa-star-o"></span>
+                </div>
+                <div class="usage">${data.productUsage}</div>
+                <div class="product-price-and-capacity">
+                    <div class="price">
+                        $${data.productPrice}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `;
+    return productHTML;
+}
+
+function mergerWithContainer(innerDom) {
+    let mainContainer = `
+    <div class="carousel" data-flickity='{ "freeScroll": true, "wrapAround": true }'>
+        ${innerDom}
+    </div>
+    `;
+    return mainContainer;
+}
+
+function insertProducts(data) {
+    let targetContainer = document.getElementById(data.categoryType);
+    let finalDOM = "";
+    data.allProducts.forEach(item => {
+        finalDOM += preapareDOM(item);
+    })
+    targetContainer.innerHTML = mergerWithContainer(finalDOM);
+}
+
+(function bindEachCategory() {
+    dataApi.forEach(item => {
+        insertProducts(item);
+    })
+})();
+
+function fetchSelectedProductData(selectedProductId, categoryType) {
+    const selectedProduct = (dataApi.filter(item => item.categoryType === categoryType))[0].allProducts.filter(item => item.id === selectedProductId)
+    return selectedProduct;
+}
+
+$(document).ready(() => {
+    $('.item-description').click(function () {
+        const panelId = $(this).closest('.panel').attr('id');
+        const unselectedProduct = $(`#${panelId}`).find('.selected').removeClass('selected');
+
+        recentUnselectedProdcut(unselectedProduct, panelId);
+
+        $(this).addClass('selected');
+
+        const productId = $(this).attr('data-product');
+        const productDetails = fetchSelectedProductData(productId, panelId);
+        placeSelectedProductinHeader(panelId, productDetails[0]);
+        updateCartPrice("addPrice", productDetails[0].productPrice);
+    })
+})
+
+function placeSelectedProductinHeader(panelId, productDetails) {
+    $(`#${panelId}`).prev().find('.product-image')[0].innerHTML = `<img src="${productDetails.productImage}" />`;
+    $(`#${panelId}`).prev().find('.product-description')[0].innerHTML = productDetails.productName;
+    let panelName = $(`#${panelId}`).prev().find('.panel-name')[0];
+    panelName.style.display = 'none';
+}
+
+function recentUnselectedProdcut(unselectedProduct, panelId) {
+    if (unselectedProduct.length > 0) {
+        const productId = unselectedProduct[0].dataset.product;
+        const productPrice = fetchSelectedProductData(productId, panelId)[0].productPrice;
+        updateCartPrice('deductPrice', productPrice)
+    } else {
+        totalSelectedProducts();
+    }
+}
+
+function totalSelectedProducts() {
+    let totalSelectedProducts = $('.noOfSelectedProducts').text();
+    totalSelectedProducts++;
+    $('.noOfSelectedProducts').text(totalSelectedProducts)
+}
+
+function updateCartPrice(actionType, productPrice) {
+    let totalPrice = $('.total-price').text();
+
+    if (actionType === "deductPrice") {
+        totalPrice = parseFloat(totalPrice) - parseFloat(productPrice);
+    } else {
+        totalPrice = parseFloat(totalPrice) + parseFloat(productPrice);
+    }
+
+    $('.total-price').text(totalPrice.toFixed(2));
+}
